91a1447d415c12c053bf64b4951a0a6ac6276651
Selqet <75011650+Selqet@users.noreply.github.com>
2020-12-09 14:47:40 +0300

    Minor fixes
    Still learning tkinter

--- GUI.py
+++ GUI.py
@@ -3,9 +3,10 @@ from PIL import ImageTk, Image
 root = tk.Tk()
 
 canv = tk.Canvas(root, width=1215, height=717, bg='white')
-canv.grid(row=2, column=3)
+canv.grid()
 
-img = ImageTk.PhotoImage(Image.open(r"F:\Python\Project\Dragontail\img\champion\splash\Azir_4.jpg"))  # PIL solution
+img = ImageTk.PhotoImage(Image.open(r".\Dragontail\img\champion\splash\Azir_4.jpg"))  # PIL solution
 canv.create_image(0, 0, anchor=tk.NW, image=img)
-tk.mainloop()
+root.mainloop()
+
 
--- Test.py
+++ Test.py
@@ -1,3 +1,18 @@
 import tkinter as tk
-root = tk.Tk()
+
+def Close():
+    startwindow.destroy()
+
+startwindow = tk.Tk()
+button = tk.Button(
+    text="Click me!",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+    command = Close
+)
+button.pack()
+startwindow.mainloop()
+
 
