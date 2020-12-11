e71cef1268a76c2ade862273e5ae401c363ab513
Selqet <75011650+Selqet@users.noreply.github.com>
2020-12-11 23:38:54 +0300

    Minimal GUI

--- Main.py
+++ Main.py
@@ -1,24 +1,82 @@
 import requests
 import json
 import Resources
-import GUI
+import tkinter as tk
+from tkinter import ttk
+from PIL import ImageTk, Image
 
-GUI.WelcomeWindow()
-api_key = GUI.apikey
+def WelcomeWindow():
 
-GUI.MainWindow()
+    def ReadAPI():
+        global api_key
+        api_key = wentry.get()
+        welcomewin.destroy()
 
-server = input('Enter server\n')
-summoner_name = input('Enter summoner name\n')
-resp = requests.get(f'https://{Resources.servers[server]}/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={api_key}')
-summoner = json.loads(resp.content)
-print(resp.content)
-summ_id = summoner['id']
-encr_id = summoner['accountId']
-puuid = summoner['puuid']
-mastery = requests.get(f'https://{Resources.servers[server]}/lol/champion-mastery/v4/champion-masteries/by-summoner/{summ_id}?api_key={api_key}')
-with open('./masterylist.json', 'w') as writef:
-    json.dump(json.loads(mastery.content), writef)
-with open(r'.\Dragontail\10.24.1\data\en_US\championFull.json', 'r') as readf:
-    data = json.load(readf)
-print(data['keys'])
+    welcomewin = tk.Tk()
+    welcomewin.geometry('400x120')
+    welcomewin.title('Welcome, Summoner')
+
+    wframe = tk.Frame(welcomewin, width = 400, height = 120)
+    wframe.pack()
+
+    wlabel = tk.Label(wframe, text = 'Enter your API key')
+    wlabel.place(x = 200, y = 10, anchor = tk.N)
+
+    wentry = tk.Entry(wframe, show = '*', width = 43)
+    wentry.place(x = 200, y = 40, anchor = tk.N)
+
+    wbutton = tk.Button(wframe, text = 'Confirm', command = ReadAPI)
+    wbutton.place(x=200, y=70, anchor = tk.N)
+
+    welcomewin.mainloop()
+
+
+def MainWindow():
+
+    def Search():
+        summoner = srchsument.get()
+        server = srchsrvr.get()
+        GetSummonerData(summoner, server)        
+
+    def SwitchSummoner():
+        pass
+
+    root = tk.Tk()
+    root.title('LeagueofLegendsProfileReviewer')
+
+    background = tk.Canvas(root, width=1215, height=717, bg='white')
+    background.grid()
+    img = ImageTk.PhotoImage(Image.open(r'.\Resources\DefaultBackground.jpg'))
+    background.create_image(0, 0, anchor=tk.NW, image=img)
+
+    searchframe = tk.Frame(root, width = 200, height = 120, bg = 'white')
+    searchframe.place(x=0, y=0)
+    srchsumlbl = tk.Label(searchframe, text = "Summoner's name", bg = 'white')
+    srchsumlbl.place(x=100, y=20, anchor = tk.N)
+    srchsument = tk.Entry(searchframe, width = 20)
+    srchsument.place(x=100, y=45, anchor = tk.N)
+    srchsrvr = ttk.Combobox(searchframe, values=['BR1','EUN','EUW','JP1','KR','LA1','LA2','NA1','OC1','TR1','RU'], width = 5)
+    srchsrvr.place(x=38, y=80)
+    srchbtn = tk.Button(searchframe, text = 'Search', command = Search)
+    srchbtn.place(x=110, y=76)
+
+    root.mainloop()
+
+
+def GetSummonerData(summoner, server):
+    resp = requests.get(f'https://{Resources.servers[server]}/lol/summoner/v4/summoners/by-name/{summoner}?api_key={api_key}')
+    summonerk = json.loads(resp.content)
+    print(resp.content)
+    summ_id = summonerk['id']
+    encr_id = summonerk['accountId']
+    puuid = summonerk['puuid']
+    mastery = requests.get(f'https://{Resources.servers[server]}/lol/champion-mastery/v4/champion-masteries/by-summoner/{summ_id}?api_key={api_key}')
+    with open('./masterylist.json', 'w') as writef:
+        json.dump(json.loads(mastery.content), writef)
+    with open(r'.\Dragontail\10.24.1\data\en_US\championFull.json', 'r') as readf:
+        data = json.load(readf)
+    print(data['keys'])
+
+WelcomeWindow()
+MainWindow()
+
--- Test.py
+++ Test.py
@@ -1,18 +1,31 @@
 import tkinter as tk
+from tkinter import ttk
 from PIL import ImageTk, Image
 
+def Search():
+    summmoner = srchsument.get()
+    server = srchsrvr.get()
+    print(summmoner)
+    print(server)
+
 root = tk.Tk()
+root.title('LeagueofLegendsProfileReviewer')
 
 background = tk.Canvas(root, width=1215, height=717, bg='white')
 background.grid()
+img = ImageTk.PhotoImage(Image.open(r'.\Resources\DefaultBackground.jpg'))
+background.create_image(0, 0, anchor=tk.NW, image=img)
 
-searchframe = tk.Frame(root, width = 200, height = 200, bg = 'white')
+searchframe = tk.Frame(root, width = 200, height = 120, bg = 'white')
 searchframe.place(x=0, y=0)
 srchsumlbl = tk.Label(searchframe, text = "Summoner's name", bg = 'white')
 srchsumlbl.place(x=100, y=20, anchor = tk.N)
-
-img = ImageTk.PhotoImage(Image.open(r'.\Resources\DefaultBackground.jpg'))
-background.create_image(0, 0, anchor=tk.NW, image=img)
+srchsument = tk.Entry(searchframe, width = 20)
+srchsument.place(x=100, y=45, anchor = tk.N)
+srchsrvr = ttk.Combobox(searchframe, values=['BR1','EUN','EUW','JP1','KR','LA1','LA2','NA1','OC1','TR1','RU'], width = 5)
+srchsrvr.place(x=38, y=80)
+srchbtn = tk.Button(searchframe, text = 'Search', command = Search)
+srchbtn.place(x=110, y=76)
 
 root.mainloop()
 r'''
