077db966e97d7cef0dc4f4a0e0ba894f7d4565c0
Selqet <75011650+Selqet@users.noreply.github.com>
2020-12-13 00:53:57 +0300

    v1.0

--- Main.py
+++ Main.py
@@ -5,11 +5,44 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
 
+def ReadAPI():
+    global api_key
+    api_key = wentry.get()
+    welcomewin.destroy()
+
+def Search():
+    summoner = srchsument.get()
+    server = srchsrvr.get()
+    GetSummonerData(summoner, server)  
+
+def SwitchSummoner(champions, masteryscore):
+    mainmasteryframe = tk.Frame(root)
+    mainmasteryframe.place(x=1215, y=0, anchor = tk.NE)
+
+    championlbl = tk.Label(mainmasteryframe, text = 'Champions')
+    championlbl.grid(row=0,column=0)
+    championlbl = tk.Label(mainmasteryframe, text = 'Champions')
+    championlbl.grid(row=0,column=3)
+    masterylbl = tk.Label(mainmasteryframe, text = 'Score')
+    masterylbl.grid(row=0,column=1)
+    masterylbl = tk.Label(mainmasteryframe, text = 'Score')
+    masterylbl.grid(row=0,column=4)
+    for i in range(10):
+        championlbl = tk.Label(mainmasteryframe, text = champions[i])
+        championlbl.grid(row=i+1,column=0)
+        masterylbl = tk.Label(mainmasteryframe, text = masteryscore[i])
+        masterylbl.grid(row=i+1,column=1)
+        emptylbl = tk.Label(mainmasteryframe, text = '   ')
+        emptylbl.grid(row=i+1,column=2)
+        championlbl = tk.Label(mainmasteryframe, text = champions[i+10])
+        championlbl.grid(row=i+1,column=3)
+        masterylbl = tk.Label(mainmasteryframe, text = masteryscore[i+10])
+        masterylbl.grid(row=i+1,column=4)
+        
+    
 def WelcomeWindow():
-    def ReadAPI():
-        global api_key
-        api_key = wentry.get()
-        welcomewin.destroy()
+    global welcomewin
+    global wentry
 
     welcomewin = tk.Tk()
     welcomewin.geometry('400x120')
@@ -31,13 +64,9 @@ def WelcomeWindow():
 
 
 def MainWindow():
-    def Search():
-        summoner = srchsument.get()
-        server = srchsrvr.get()
-        GetSummonerData(summoner, server)        
-
-    def SwitchSummoner():
-        pass
+    global root
+    global srchsument
+    global srchsrvr
 
     root = tk.Tk()
     root.title('LeagueofLegendsProfileReviewer')
@@ -65,14 +94,24 @@ def GetSummonerData(summoner, server):
     sumbynameresp = requests.get(f'https://{Resources.servers[server]}/lol/summoner/v4/summoners/by-name/{summoner}?api_key={api_key}')
     summonerdata = json.loads(sumbynameresp.content)
     summ_id = summonerdata['id']
-    encr_id = summonerdata['accountId']
-    puuid = summonerdata['puuid']
     masteryresp = requests.get(f'https://{Resources.servers[server]}/lol/champion-mastery/v4/champion-masteries/by-summoner/{summ_id}?api_key={api_key}')
     mastery = json.loads(masteryresp.content)
-    for champion in mastery:
-        print(champion['championId'])
+    #for champion in mastery:
+        #print(champion['championId'])
     with open(r'.\Dragontail\10.24.1\data\en_US\championFull.json', 'r') as readf:
-        data = json.load(readf)
+        championfulldata = json.load(readf)
+
+    keys = championfulldata['keys']
+    champions = []
+    masteryscore = []
+    for champion in mastery[:20]:
+        masteryscore.append(champion['championPoints'])
+        championkey = str(champion['championId'])
+        champions.append(keys[championkey])
+
+    SwitchSummoner(champions, masteryscore)
+
+
 
 WelcomeWindow()
 MainWindow()
--- Test.py
+++ Test.py
@@ -1,33 +1,28 @@
+import requests
+import json
+import Resources
 import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
 
 def Search():
-    summmoner = srchsument.get()
-    server = srchsrvr.get()
-    print(summmoner)
-    print(server)
+    pass
 
-root = tk.Tk()
-root.title('LeagueofLegendsProfileReviewer')
+sumbynameresp = requests.get(f'https://{Resources.servers[server]}/lol/summoner/v4/summoners/by-name/{summoner}?api_key={api_key}')
+summonerdata = json.loads(sumbynameresp.content)
+summ_id = summonerdata['id']
+masteryresp = requests.get(f'https://{Resources.servers[server]}/lol/champion-mastery/v4/champion-masteries/by-summoner/{summ_id}?api_key={api_key}')
+mastery = json.loads(masteryresp.content)
+for champion in mastery:
+    print(champion['championId'])
+with open(r'.\Dragontail\10.24.1\data\en_US\championFull.json', 'r') as readf:
+    data0 = json.load(readf)
+for champion in mastery[:10]:
+    masteryscore = []
+    masteryscore.append(champion['championPoints'])
+print(data0)
+print(masteryscore)
 
-background = tk.Canvas(root, width=1215, height=717, bg='white')
-background.grid()
-img = ImageTk.PhotoImage(Image.open(r'.\Resources\DefaultBackground.jpg'))
-background.create_image(0, 0, anchor=tk.NW, image=img)
-
-searchframe = tk.Frame(root, width = 200, height = 120, bg = 'white')
-searchframe.place(x=0, y=0)
-srchsumlbl = tk.Label(searchframe, text = "Summoner's name", bg = 'white')
-srchsumlbl.place(x=100, y=20, anchor = tk.N)
-srchsument = tk.Entry(searchframe, width = 20)
-srchsument.place(x=100, y=45, anchor = tk.N)
-srchsrvr = ttk.Combobox(searchframe, values=['BR1','EUN','EUW','JP1','KR','LA1','LA2','NA1','OC1','TR1','RU'], width = 5)
-srchsrvr.place(x=38, y=80)
-srchbtn = tk.Button(searchframe, text = 'Search', command = Search)
-srchbtn.place(x=110, y=76)
-
-root.mainloop()
 r'''
 img = ImageTk.PhotoImage(Image.open(r".\Dragontail\img\champion\splash\Azir_4.jpg"))  # PIL solution
 background.create_image(0, 0, anchor=tk.NW, image=img)
@@ -41,7 +36,7 @@ img = ImageTk.PhotoImage(Image.open(r".\Dragontail\img\champion\splash\Azir_4.jp
 canv.create_image(0, 0, anchor=tk.NW, image=img)
 root.mainloop()
 '''
-'''
+r'''
 import tkinter as tk
 import Main
 from tkinter import ttk
@@ -107,4 +102,10 @@ def MainWindow():
 '''
     with open('./masterylist.json', 'w') as writef:
         json.dump(json.loads(mastery.content), writef)
+'''
+
+'''
+    summ_id = summonerdata['id']
+    encr_id = summonerdata['accountId']
+    puuid = summonerdata['puuid']
 '''
